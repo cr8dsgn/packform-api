ab4fd5d607279f2da6b547053482218f93aaa092
Cr8.Dsgn <cr8.dsgn@gmail.com>
2026-07-10 11:32:36 +0500

    Enable CORS

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const buildRoute = require("./routes/build");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/api/build", buildRoute);
 
 app.get("/health", (req, res) => {
